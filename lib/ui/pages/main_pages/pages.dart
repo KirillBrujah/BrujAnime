4666fa6bdbd6651d0de38d823d09379087fdaa52
krioproton2 <krioproton2@protonmail.com>
2023-01-11 11:47:31 +0100

    Add some pages to app_router. Didn't build.

---
+++ lib/ui/pages/main_pages/pages.dart
@@ -0,0 +1,5 @@
+export 'catalog_pages/pages.dart';
+export 'favorite_pages/pages.dart';
+export 'home_pages/pages.dart';
+export 'main_page.dart';
+export 'settings_pages/pages.dart';
--- lib/utils/app_router.dart
+++ lib/utils/app_router.dart
@@ -1,10 +1,62 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:auto_route/empty_router_widgets.dart';
+import 'package:brujanime/ui/pages/main_pages/pages.dart';
 import 'package:brujanime/ui/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 
 part 'app_router.gr.dart';
 
+const _homeTab = AutoRoute(
+  name: "HomeTabRoute",
+  initial: true,
+  page: EmptyRouterPage,
+  children: [
+    //
+  ],
+);
+
+const _catalogTab = AutoRoute(
+  name: "CatalogTabRoute",
+  page: EmptyRouterPage,
+  children: [
+    AutoRoute(
+      page: CatalogPage,
+      initial: true,
+    ),
+  ],
+);
+
+const _favoriteTab = AutoRoute(
+  name: "FavoriteTabRoute",
+  page: EmptyRouterPage,
+  children: [
+    //
+  ],
+);
+
+const _settingsTab = AutoRoute(
+  name: "SettingsTabRoute",
+  page: EmptyRouterPage,
+  children: [
+    //
+  ],
+);
+
+const _mainRoute = AutoRoute(
+  page: MainPage,
+  children: [
+    _homeTab,
+    _catalogTab,
+    _favoriteTab,
+    _settingsTab,
+  ],
+);
+
 @MaterialAutoRouter(
-    replaceInRouteName: 'Page,Route',
-    routes: <AutoRoute>[AutoRoute(page: SplashPage, initial: true)])
+  replaceInRouteName: 'Page,Route',
+  routes: <AutoRoute>[
+    AutoRoute(page: SplashPage, initial: true),
+    _mainRoute,
+  ],
+)
 class AppRouter extends _$AppRouter {}
