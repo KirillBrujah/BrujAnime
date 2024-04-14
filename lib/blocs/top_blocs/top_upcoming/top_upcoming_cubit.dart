a7b0792a2195dc844d613b5c3792173cd7dc801a
KirillBrujah <kirillbrujah@gmail.com>
2024-04-15 00:11:13 +0200

    Add using TopUpcomingLoadingCubit type to top_upcoming_cubit.dart

--- lib/blocs/top_blocs/top_upcoming/top_upcoming_cubit.dart
+++ lib/blocs/top_blocs/top_upcoming/top_upcoming_cubit.dart
@@ -4,6 +4,7 @@ import 'package:brujanime/generated/l10n.dart';
 import 'package:brujanime/models/models.dart';
 import 'package:brujanime/utils/debug_functions.dart';
 import 'package:brujanime/utils/getit.dart';
+import 'package:brujanime/utils/typedefs.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -37,7 +38,7 @@ class TopUpcomingCubit extends Cubit<TopUpcomingState> {
 
   late final TopRepository topRepository;
 
-  final DataLoadingCubit<TopUpcomingCubit, List<Anime>> loadingCubit;
+  final TopUpcomingLoadingCubit loadingCubit;
 
   Future<ApiResultWrapper<List<Anime>>> _fetch({required int page}) async {
     final results = await topRepository.getAiring(page: page);
